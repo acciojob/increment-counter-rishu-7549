48c1456c0a25279356bca3ec1fb6945f01e9a10a
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-05 14:34:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let count = document.getElementById("counter");
+let incre = document.getElementById("incrementBtn");
+
+incre.addEventListener("click", function () {
+  let currentValue = parseInt(count.innerHTML);
+  alert(currentValue);
+  count.innerHTML = currentValue + 1;
+});
+
